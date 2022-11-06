c49e88d6c06f7451af45962db54e2d4f07dc1061
Elio Raphael <69924027+elio75006@users.noreply.github.com>
2022-11-06 21:09:29 +0100

    argparse

--- password_generator.py
+++ password_generator.py
@@ -4,16 +4,42 @@ import argparse
 import sys
 import pyperclip
 
+
 def main():
     parser = argparse.ArgumentParser(description="Generate a random password")
-    parser.add_argument('-n', default=8, help="Specify the length of your password", type=int)
-    parser.add_argument('-a', '-all', help="Generate password with ascii, digits and punctuations", action="store_true")
-    parser.add_argument('-c', '-ascii', help="Add ascii letters to the generation pool | 0 for lower case | 1 for upper case | 2 for both", default=2, type=int, choices=[0, 1, 2])
-    parser.add_argument('-i', '-digit', '-d', help="Add digits to the generation pool", action="store_true")
-    parser.add_argument('-p', '-punctuation', help="Add punctuation to the generation pool", action="store_true")
+    parser.add_argument(
+        "-n", default=8, help="Specify the length of your password", type=int
+    )
+    parser.add_argument(
+        "-a",
+        "-all",
+        help="Generate password with ascii, digits and punctuations",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c",
+        "-ascii",
+        help="Add ascii letters to the generation pool | 0 for lower case | 1 for upper case | 2 for both",
+        default=2,
+        type=int,
+        choices=[0, 1, 2],
+    )
+    parser.add_argument(
+        "-i",
+        "-digit",
+        "-d",
+        help="Add digits to the generation pool",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-p",
+        "-punctuation",
+        help="Add punctuation to the generation pool",
+        action="store_true",
+    )
     args = parser.parse_args()
 
-    chars = ''
+    chars = ""
     all_chars = string.ascii_letters + string.digits + string.punctuation
     if args.a:
         chars = string.ascii_letters + string.digits + string.punctuation
@@ -24,25 +50,21 @@ def main():
         chars += string.ascii_uppercase
     if args.c == 2:
         chars += string.ascii_letters
-    
+
     if args.i:
         chars += string.digits
-    
+
     if args.p:
         chars += string.punctuation
-    
+
     if len(sys.argv) != 1:
         generator(chars, args.n)
     else:
         generator(all_chars, args.n)
 
-    
-    
-
-    
 
 def generator(character_list, length):
-    password = ''
+    password = ""
     for i in range(length):
         password = password + random.choice(character_list)
     pyperclip.copy(password)
